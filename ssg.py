8b519ec37ec73fd0d1c1ca548eb4435b0d9236e3
jvrmlndz <melendezdroz@gmail.com>
2022-06-03 09:22:00 -0400

    site class module

--- ssg.py
+++ ssg.py
@@ -1,2 +1,11 @@
+from distutils.command.config import config
 import typer
 from ssg.site import Site
+
+def main(source = "content", dest = "dist"):
+    config = {"source": source, "dest": dest}
+
+    Site(**config).build()
+
+
+typer.run(main)
