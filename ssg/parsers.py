4176f54fd9b563debe153b4dd7eede58b6e6c0d0
jvrmlndz <melendezdroz@gmail.com>
2022-06-03 12:35:53 -0400

    .

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -42,7 +42,8 @@ class MarkdownParser:
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
-        
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
 
     
 
